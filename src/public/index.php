ad34880b86c33347b03253a6933a5959f5811810
MinhThi05 <minhthix05@gmail.com>
2025-10-26 00:03:32 +0700

    Add files via upload

---
+++ src/public/index.php
@@ -0,0 +1,8 @@
+<?php
+// Gọi phần header
+include_once __DIR__ . '/../app/views/templates/header.php';
+include_once __DIR__ . '/../app/views/home.php';
+include_once __DIR__ . '/../app/views/templates/footer.php';
+//include_once __DIR__ . '/../app/views/login.php';
+//include_once __DIR__ . '/../app/views/sign_up.php';
+?>
