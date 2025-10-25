c260d20cc09e313718cefd65caab01278497964b
minhthu2010 <minhthux005@gmail.com>
2025-10-25 23:56:01 +0700

    Create footer.php

---
+++ src/app/views/templates/footer.php
@@ -0,0 +1,49 @@
+
+<footer class="footer py-5">
+  <div class="container">
+    <div class="row">
+      <!-- Cột 1: Logo / Tên thương hiệu -->
+      <div class="col-md-3 mb-4">
+        <h4 class="fw-normal">D. Patel</h4>
+      </div>
+
+      <!-- Cột 2: Liên kết trang -->
+      <div class="col-md-2 mb-4">
+        <ul class="list-unstyled small">
+          <li><a href="#" class="text-decoration-none">Shop All</a></li>
+          <li><a href="#" class="text-decoration-none">About</a></li>
+          <li><a href="#" class="text-decoration-none">Contact</a></li>
+        </ul>
+      </div>
+
+      <!-- Cột 3: Chính sách -->
+      <div class="col-md-3 mb-4">
+        <ul class="list-unstyled small">
+          <li><a href="#" class="text-decoration-none">Privacy Policy</a></li>
+          <li><a href="#" class="text-decoration-none">Accessibility Statement</a></li>
+          <li><a href="#" class="text-decoration-none">Shipping Policy</a></li>
+          <li><a href="#" class="text-decoration-none">Terms & Conditions</a></li>
+          <li><a href="#" class="text-decoration-none">Refund Policy</a></li>
+        </ul>
+      </div>
+
+      <!-- Cột 4: Thông tin liên hệ -->
+      <div class="col-md-4 mb-4 small">
+        <p class="mb-1">123-456-7890</p>
+        <p class="mb-1">info@mysite.com</p>
+        <p>500 Terry Francine St.<br>San Francisco, CA 94158</p>
+
+        <div class="d-flex gap-3 mt-2">
+          <a href="#" class="text-dark"><i class="fa-brands fa-instagram"></i></a>
+          <a href="#" class="text-dark"><i class="fa-brands fa-facebook-f"></i></a>
+          <a href="#" class="text-dark"><i class="fa-brands fa-twitter"></i></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="text-center mt-4 small">
+      © 2035 by D. Patel. Powered 
+    </div>
+  </div>
+</footer>
+
