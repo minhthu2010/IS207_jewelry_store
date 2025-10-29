1ae973f99013b2c0a17f5b2f6f614ce31e4532ed
minhthu2010 <minhthux005@gmail.com>
2025-10-29 22:14:06 +0700

    Update logout.php

--- src/public/logout.php
+++ src/public/logout.php
@@ -3,6 +3,11 @@ session_start();
 session_unset();
 session_destroy();
 
+// Xóa cookie token
+if (isset($_COOKIE['token'])) {
+    setcookie('token', '', time() - 3600, "/");
+}
+
 header('Content-Type: application/json');
 echo json_encode(['status' => 'success']);
 exit();
