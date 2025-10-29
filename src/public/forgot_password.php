30a02645c786c58059dbf4dce2613990415ab606
minhthu2010 <minhthux005@gmail.com>
2025-10-29 22:13:27 +0700

    Update forgot_password.php

--- src/public/forgot_password.php
+++ src/public/forgot_password.php
@@ -1,26 +1,7 @@
 <?php
-session_start();
+require_once '../config/config.php';
+require_once '../app/controllers/forgotPasswordController.php';
 
-
-require_once __DIR__ . '/../app/controllers/forgotPasswordController.php';
-
-$controller = new ForgotPasswordController();
-
-if (isset($_GET['action'])) {
-    switch ($_GET['action']) {
-        case 'send_reset_link':
-            $controller->sendResetLink();
-            break;
-        case 'verify_otp':
-            $controller->verifyOtp();
-            break;
-        case 'reset_password':
-            $controller->resetPassword();
-            break;
-        default:
-            $controller->showForm();
-    }
-} else {
-    $controller->showForm();
-}
+$controller = new ForgotPasswordController($conn);
+$controller->handleRequest();
 ?>
