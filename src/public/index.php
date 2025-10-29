b0a098275a7007b49289a9d939dd411c1af85ed3
MinhThi05 <minhthix05@gmail.com>
2025-10-29 23:04:13 +0700

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,48 @@
 <?php
-// Gọi phần header
-include_once __DIR__ . '/../app/views/home.php';
-//include_once __DIR__ . '/../app/views/login.php';
-//include_once __DIR__ . '/../app/views/sign_up.php';
+// public/index.php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
+require_once __DIR__ . '/../config/config.php';
+
+$action = $_GET['action'] ?? 'list';
+
+switch ($action) {
+    case 'list':
+        require_once __DIR__ . '/../app/controllers/productController.php';
+        $controller = new productController($conn);
+        $controller->list();
+        break;
+        
+    case 'detail':
+        require_once __DIR__ . '/../app/controllers/productController.php';
+        $controller = new productController($conn);
+        $id = $_GET['id'] ?? 0;
+        $controller->detail($id);
+        break;
+        
+    case 'cart':  // THÊM CASE NÀY
+        require_once __DIR__ . '/../app/controllers/cartController.php';
+        $controller = new CartController($conn);
+        $controller->viewCart();
+        break;
+        
+    case 'login':
+        require_once __DIR__ . '/../app/controllers/loginController.php';
+        $controller = new LoginController($conn);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controller->login();
+        } else {
+            include __DIR__ . '/../app/views/login.php';
+        }
+        break;
+        
+    default:
+        require_once __DIR__ . '/../app/controllers/productController.php';
+        $controller = new productController($conn);
+        $controller->list();
+        break;
+}
 ?>
