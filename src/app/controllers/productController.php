15059cd29e466d63990fa635c6834e51c2fdf776
MinhThi05 <minhthix05@gmail.com>
2025-10-30 22:11:59 +0700

    Update productController.php

--- src/app/controllers/productController.php
+++ src/app/controllers/productController.php
@@ -1,16 +1,56 @@
 <?php
-// app/controllers/productController.php
+require_once __DIR__ . '/../../config/config.php';
 require_once __DIR__ . '/../models/product.php';
+require_once __DIR__ . '/../models/cart.php';
 
-class ProductController {
-    public function list() {
-        $products = Product::getAllProducts();
-        include __DIR__ . '/../views/product.php';
+class productController {
+    private $productModel;
+    private $cartModel;
+
+    public function __construct($db) {
+        $this->productModel = new Product($db);
+        $this->cartModel = new CartModel($db);
     }
 
     public function detail($id) {
-        $product = Product::getProductById($id);
+    $product = $this->productModel->getProductById($id);
+    $cartItemCount = $this->getCartItemCount();
+    
+    if ($product) {
+        // Lấy variants
+        $product['variants'] = $this->productModel->getVariantsByProduct($id);
+        
+        // DEBUG
+        error_log("Product Detail - ID: $id");
+        error_log("Product Name: " . ($product['name'] ?? 'N/A'));
+        error_log("Variants Count: " . count($product['variants']));
+        foreach ($product['variants'] as $v) {
+            error_log("Variant: ID=" . $v['variant_id'] . ", Size=" . ($v['size'] ?? 'N/A'));
+        }
+        
         include __DIR__ . '/../views/product_detail.php';
+    } else {
+        include __DIR__ . '/../views/404.php';
+    }
+}
+
+    public function list() {
+        $products = $this->productModel->getAllProducts();
+        $cartItemCount = $this->getCartItemCount();
+        include __DIR__ . '/../views/product.php';
+    }
+
+   
+
+    // Lấy số lượng sản phẩm trong giỏ hàng
+    private function getCartItemCount() {
+        if (isset($_SESSION['customer']['cus_id'])) {
+            $cart = $this->cartModel->getCartByCustomerId($_SESSION['customer']['cus_id']);
+            if ($cart) {
+                return $this->cartModel->getCartItemCount($cart['cart_id']);
+            }
+        }
+        return 0;
     }
 }
 ?>
