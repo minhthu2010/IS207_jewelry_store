d1303b52b7a722b15d97409a54b3d87ad7eb7133
minhthu2010 <minhthux005@gmail.com>
2025-11-15 21:27:46 +0700

    Update reset_password.php

--- src/app/views/reset_password.php
+++ src/app/views/reset_password.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reset Password</title>
+    <title>Đặt lại mật khẩu</title>
     <link rel="stylesheet" href="../public/assets/css/bootstrap-5.3.8-dist/bootstrap-5.3.8-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../public/assets/css/style_login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
@@ -11,12 +11,12 @@
 <body class="login">
     <div class="login-container">
         <div class="login-left">
-            <h1>Set New Password</h1>
-            <p>Create your new password.</p>
+            <h1>Đặt mật khẩu mới</h1>
+            <p>Tạo mật khẩu mới của bạn.</p>
         </div>
 
         <div class="login-right">
-            <h2>New Password</h2>
+            <h2>Mật khẩu mới</h2>
 
             <?php if (!empty($error)): ?>
                 <div class="alert alert-danger"><?= htmlspecialchars($error) ?></div>
@@ -29,18 +29,18 @@
                 
                 <div class="input-group">
                     <i class="fa-solid fa-lock icon"></i>
-                    <input type="password" name="new_password" class="form-control" placeholder="New Password" required />
+                    <input type="password" name="new_password" class="form-control" placeholder="Mật khẩu mới" required />
                 </div>
 
                 <div class="input-group">
                     <i class="fa-solid fa-lock icon"></i>
-                    <input type="password" name="confirm_password" class="form-control" placeholder="Confirm New Password" required />
+                    <input type="password" name="confirm_password" class="form-control" placeholder="Xác nhận mật khẩu mới" required />
                 </div>
 
-                <button type="submit" class="btn-login w-100">Reset Password</button>
+                <button type="submit" class="btn-login w-100">Đặt lại mật khẩu</button>
 
                 <p class="signup-text">
-                    <a href="login.php">Back to Login</a>
+                    <a href="login.php">Quay về trang Đăng nhập</a>
                 </p>
             </form>
         </div>
