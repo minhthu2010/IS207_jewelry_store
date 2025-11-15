01fc62e3f859395b8c68f0ea1aa63e3da72c6162
minhthu2010 <minhthux005@gmail.com>
2025-11-15 21:27:06 +0700

    Update login.php

--- src/app/views/login.php
+++ src/app/views/login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Login Page</title>
+  <title>Đăng nhập</title>
   <link rel="stylesheet" href="../public/assets/css/bootstrap-5.3.8-dist/bootstrap-5.3.8-dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../public/assets/css/style_login.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
@@ -12,45 +12,45 @@
 <body class="login">
   <div class="login-container">
     <div class="login-left">
-      <h1>Welcome back!</h1>
-      <p>You can log in to access with your existing account.</p>
+      <h1>Chào mừng bạn!</h1>
+      <p>Đăng nhập tham gia với chúng tôi.</p>
     </div>
 
     <div class="login-right">
-      <h2>Log In</h2>
-      <?php if (!empty($error)): ?>
-  <div class="alert alert-danger error-message">
-      <?= htmlspecialchars($error) ?>
-  </div>
+      <h2>Đăng nhập</h2>
+        <?php if (!empty($error)): ?>
+          <div class="alert alert-danger error-message">
+        <?= htmlspecialchars($error) ?>
+    </div>
 <?php endif; ?>
 
 
 
 
       <form method="POST" action="">
-  <div class="input-group">
-    <i class="fa-solid fa-user icon"></i>
-    <input type="text" name="email" class="form-control" placeholder="Email" required />
-  </div>
+        <div class="input-group">
+          <i class="fa-solid fa-user icon"></i>
+          <input type="text" name="email" class="form-control" placeholder="Email" required />
+        </div>
 
-  <div class="input-group">
-    <i class="fa-solid fa-lock icon"></i>
-    <input type="password" name="password" class="form-control" placeholder="Password" required />
-  </div>
+        <div class="input-group">
+          <i class="fa-solid fa-lock icon"></i>
+          <input type="password" name="password" class="form-control" placeholder="Mật khẩu" required />
+        </div>
 
-  <div class="options">
-  <div>
-    <input type="checkbox" id="remember" name="remember" />
-    <label for="remember">Remember me</label>
-  </div>
-  <a href="forgot_password.php">Forgot password?</a> 
-</div>
+        <div class="options">
+        <div>
+          <input type="checkbox" id="remember" name="remember" />
+          <label for="remember">Nhớ tài khoản</label>
+        </div>
+        <a href="forgot_password.php">Quên mật khẩu?</a> 
+      </div>
 
 
-  <button type="submit" class="btn-login w-100">Log In</button>
+  <button type="submit" class="btn-login w-100">Đăng nhập</button>
 
   <p class="signup-text">
-    New here? <a href="sign_up.php">Create an Account</a>
+    Khách hàng mới? <a href="sign_up.php">Đăng ký tài khoản</a>
   </p>
 </form>
 
