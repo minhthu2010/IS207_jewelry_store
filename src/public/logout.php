38f059fffb5727bf9a03ee537e59b2170166896a
minhthu2010 <minhthux005@gmail.com>
2025-11-15 21:29:02 +0700

    Update logout.php

--- src/public/logout.php
+++ src/public/logout.php
@@ -3,12 +3,15 @@ session_start();
 session_unset();
 session_destroy();
 
-// Xóa cookie token
+// Xóa cookie token nếu có
 if (isset($_COOKIE['token'])) {
     setcookie('token', '', time() - 3600, "/");
 }
 
+// Trả JSON cho fetch JS
 header('Content-Type: application/json');
-echo json_encode(['status' => 'success']);
+echo json_encode([
+    'status' => 'success',
+    'redirect' => 'index.php' 
+]);
 exit();
-?>
