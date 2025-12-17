5a2194a1e8acfb9f5a335603b77a9e731fecbac0
minhthu2010 <minhthux005@gmail.com>
2025-12-18 01:50:22 +0700

    Update index.php

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,3 +1,16 @@
 <?php
+session_start();
+
+
+// Kiểm tra xem admin đã đăng nhập chưa
+if (!isset($_SESSION['admin'])) {
+    // Chưa đăng nhập -> chuyển sang trang login
+    header("Location: login.php");
+    exit;
+}
+
+// Nếu đã đăng nhập -> load dashboard
 require_once 'controllers/dashboardController.php';
+$controller = new DashboardController($conn);
+$controller->index();
 ?>
